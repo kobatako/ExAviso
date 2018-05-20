7adc64b77849fb4e1064c6e88adaff8dc1acb90b
kobatako <ju.koba.itit.shar@gmail.com>
2018-05-20 05:19:52 +0000

    first commit

---
+++ lib/ex_aviso.ex
@@ -0,0 +1,18 @@
+defmodule ExAviso do
+  @moduledoc """
+  Documentation for ExAviso.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExAviso.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_aviso_test.exs
@@ -0,0 +1,8 @@
+defmodule ExAvisoTest do
+  use ExUnit.Case
+  doctest ExAviso
+
+  test "greets the world" do
+    assert ExAviso.hello() == :world
+  end
+end
