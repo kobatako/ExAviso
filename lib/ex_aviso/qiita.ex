7e173a6094d52c4c94784793434de29e95b6de3e
KobaruTakahiro <kobaru@fusic.co.jp>
2018-05-28 08:48:21 +0900

    fetch qiita tag feed

--- lib/ex_aviso/qiita.ex
+++ lib/ex_aviso/qiita.ex
@@ -2,6 +2,7 @@ defmodule ExAviso.Qiita do
 
 	@default_get_my_items_option %{"page" => 1, "per_page" => 20}
 	@default_get_items_option %{"user" => "", "page" => 1, "per_page" => 20}
+	@default_get_tag_items_option %{"page" => 1, "per_page" => 20}
 
   def callback_handle_get_my_items(:message, _) do
 		:ok
@@ -34,6 +35,22 @@ defmodule ExAviso.Qiita do
     }}
   end
 
+  def callback_handle_get_tag_items(:message, _) do
+		:ok
+	end
+
+  def callback_handle_get_tag_items(:desktop_notification, m) do
+    text = String.split(m.content, "\n")
+    |> get_tag_items()
+
+    {:send, %ExAviso.SlackRequest{
+      type: "message",
+      channel: m.channel,
+      text: Enum.join(text, "\n"),
+      ts: DateTime.to_unix(DateTime.utc_now()),
+    }}
+  end
+
   def get_items([]) do
     []
   end
@@ -55,14 +72,13 @@ defmodule ExAviso.Qiita do
       end
      
       e = for %{title: title, url: url} <- body, do: "#{title}[#{url}]"
-      ["-----------------------------------------------------", Enum.join(e, "\n")]
+      [Enum.join(e, "\n")]
     else
       [""]
     end
   end
 
   def get_items([head| tail]) do
-    IO.inspect head
     resp = if Regex.match?(~r/getItems/, head) do
 			option = ExAviso.Slack.get_option(head, @default_items_option)
 
@@ -76,8 +92,7 @@ defmodule ExAviso.Qiita do
           IO.inspect error
       end
      
-      e = for %{title: title, url: url} <- body, do: title <> "[" <> url <> "]"
-      
+      e = for %{title: title, url: url} <- body, do: "#{title}[#{url}]"
       Enum.join([e|["----------------------------------------"]], "\n")
     else
       ""
@@ -104,7 +119,7 @@ defmodule ExAviso.Qiita do
       end
      
       e = for %{title: title, url: url} <- body, do: "#{title}[#{url}]"
-      ["-----------------------------------------------------", Enum.join(e, "\n")]
+      [Enum.join(e, "\n")]
     else
       [""]
     end
@@ -124,13 +139,59 @@ defmodule ExAviso.Qiita do
           IO.inspect error
       end
      
-      e = for %{title: title, url: url} <- body, do: title <> "[" <> url <> "]"
-      
+      e = for %{title: title, url: url} <- body, do: "#{title}[#{url}]"
       Enum.join([e|["----------------------------------------"]], "\n")
     else
       ""
     end
     [resp| get_my_items(tail)]
+  end
+
+  def get_tag_items([]) do
+    []
+  end
+
+  def get_tag_items([head| []]) do
+    if Regex.match?(~r/getTagItems/, head) do
+			option = ExAviso.Slack.get_option(head, @default_get_tag_items_option)
+
+      [token: token] = Application.get_all_env(:qiita) 
+      headers = [{"Authorization", "Bearer #{token}"}]
+      url = "https://qiita.com/api/v2/tags/#{option["tag"]}/items?page=#{option["page"]}&per_page=#{option["per_page"]}"
+      body = case HTTPoison.get!(url, headers) do
+        %{status_code: 200, body: body} ->
+          Poison.Parser.parse!(body, keys: :atoms)
+        %{error: "account_inactive"} = error ->
+          IO.inspect error
+      end
+     
+      e = for %{title: title, url: url} <- body, do: "#{title}[#{url}]"
+      [Enum.join(e, "\n")]
+    else
+      [""]
+    end
+  end
+
+  def get_tag_items([head| tail]) do
+    resp = if Regex.match?(~r/getTagItems/, head) do
+			option = ExAviso.Slack.get_option(head, @default_get_tag_items_option)
+
+      [token: token] = Application.get_all_env(:qiita) 
+      headers = [{"Authorization", "Bearer #{token}"}]
+      url = "https://qiita.com/api/v2/tags/#{option["tag"]}/items?page=#{option["page"]}&per_page=#{option["per_page"]}"
+      body = case HTTPoison.get!(url, headers) do
+        %{status_code: 200, body: body} ->
+          Poison.Parser.parse!(body, keys: :atoms)
+        %{error: "account_inactive"} = error ->
+          IO.inspect error
+      end
+     
+      e = for %{title: title, url: url} <- body, do: "#{title}[#{url}]"
+      Enum.join([e|["----------------------------------------"]], "\n")
+    else
+      ""
+    end
+    [resp| get_tag_items(tail)]
   end
 end
 
